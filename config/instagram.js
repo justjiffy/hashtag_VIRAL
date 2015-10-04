974b177a9063d2399ef0fe4053611b6f94d8196f
Juli Emmel <justjiffy@gmail.com>
2015-10-04 12:46:00 -0700

    top popular instagram photo shows on index!

--- config/instagram.js
+++ config/instagram.js
@@ -4,9 +4,10 @@ Instagram.set('client_id', 'cd52513b4ad04996b712dd40ff523962');
 Instagram.set('client_secret', '0ebac9c429a54249bcaa5b5d67e88b15');
 
 
-module.exports = Instagram.media.popular({
+module.exports = instagrams = Instagram.media.popular({
   complete: function(data){
-    console.log(data[0].caption.text);
+    image = data[0].images.thumbnail.url;
+    console.log(image);
   }
 });
 
