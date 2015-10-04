2a134aef9f491b234b63c8e8203648cb849064ab
Juli Emmel <justjiffy@gmail.com>
2015-10-03 21:42:43 -0700

    can get text lines from most popular Instagram posts

--- config/instagram.js
+++ config/instagram.js
@@ -3,10 +3,17 @@ Instagram = require('instagram-node-lib');
 Instagram.set('client_id', 'cd52513b4ad04996b712dd40ff523962');
 Instagram.set('client_secret', '0ebac9c429a54249bcaa5b5d67e88b15');
 
-module.exports = Instagram.tags.search({
-  q: 'banana',
-  count: 5,
+
+module.exports = Instagram.media.popular({
   complete: function(data){
-    console.log(data);
+    console.log(data[0].caption.text);
   }
 });
+
+// module.exports = Instagram.tags.search({
+//   q: 'banana',
+//   count: 1,
+//   complete: function(data){
+//     console.log(data);
+//   }
+// });
