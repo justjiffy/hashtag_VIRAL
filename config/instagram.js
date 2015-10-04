24308c06cf163fed15a0a2d5ee7157a95f3589af
Juli Emmel <justjiffy@gmail.com>
2015-10-03 21:31:31 -0700

    instagram returning 5 searches on banana

--- config/instagram.js
+++ config/instagram.js
@@ -3,8 +3,9 @@ Instagram = require('instagram-node-lib');
 Instagram.set('client_id', 'cd52513b4ad04996b712dd40ff523962');
 Instagram.set('client_secret', '0ebac9c429a54249bcaa5b5d67e88b15');
 
-module.exports = Instagram.tags.info({
-  name: 'blue',
+module.exports = Instagram.tags.search({
+  q: 'banana',
+  count: 5,
   complete: function(data){
     console.log(data);
   }
