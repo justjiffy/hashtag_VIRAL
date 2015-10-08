50943a4b813f810ad696a35b7455bcc641fa69f2
Justin Hong <justinhong.j@gmail.com>
2015-10-07 17:03:15 -0700

    load most popular posts

--- controllers/feeds.js
+++ controllers/feeds.js
@@ -42,5 +42,35 @@ feedsController.get('/feeds/search', function(req, res) {
 
 });
 
+feedsController.get('/feeds', function(req, res) {
+	// var search = req.query.search;
+	var twitter = {name: 'twitter', feed: []};
+	var instagram = {name: 'instagram', feed: []};
+	var youtube = {name: 'youtube', feed: []};
+
+	T.get('search/tweets', { q: '&nbsp;', result_type: 'popular', count: 20, language: 'en'},
+		function(err, data, response) {
+			var tweets_list = data.statuses;
+			// res.json(tweets_list);
+
+			twitter.feed = tweets_list;
+
+			request('https://api.instagram.com/v1/media/popular?count=20&client_id=cd52513b4ad04996b712dd40ff523962', function(error, response, body) {
+				var grams = JSON.parse(body);
+				instagram.feed = grams;
+
+				request('https://www.googleapis.com/youtube/v3/videos?part=snippet&order=rating&maxResults=5&chart=mostPopular&key=AIzaSyCbVPyvwNghVUScGrY6l-vEc4R_O5msLAU', function(error, response, body) {
+					var videos = JSON.parse(body);
+					youtube.feed = videos;
+					
+					var allArray = [twitter, instagram, youtube];
+					res.json(allArray);
+				});
+			});
+	});
+
+
+});
+
 
 module.exports = feedsController;
