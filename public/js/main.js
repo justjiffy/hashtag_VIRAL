a7170cf7ea317fec83987bed7090a324766aa1ce
Juli Emmel <justjiffy@gmail.com>
2015-10-08 01:26:57 -0700

    tweets and instagrams are properly framed

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
 	$.get('feeds/', function(data) {
 		for (i=0; i<5; i++) {
-			$('#test').append( data[0].feed[i].text + "<br>");
+			$('#test').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i].text + "</p>" );
 		}
 	});
 });
@@ -14,9 +14,9 @@ $('#search-button').click(function(e) {
 	
 		for (i=0; i<20; i++) {
 			$('#test').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i].text + "</p>" );
-			$('#img_test').append("data[1].feed[i].images.thumbnail.url");
+			$('#test').append( "<img class='instagram-pic' style='background-image: url(" + data[1].feed.data[i].images.thumbnail.url + ")' src='images/polaroid.png' />" );
 		}
-	
+
 	});
 });
 
