c88464672897fd8917c5220f436bc45d1eebe2f5
Juli Emmel <justjiffy@gmail.com>
2015-10-08 20:35:47 -0700

    added footer and grass

--- public/js/main.js
+++ public/js/main.js
@@ -11,14 +11,19 @@ $(document).ready(function() {
 			gram2 = data[1].feed.data[i+1];
 			$('#three').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i+2].text + "</p>" );
 			three = data[0].feed[i+2];
-			$('#gram3').append( "<img class='instagram-pic tilt_15' style='background-image: url(" + data[1].feed.data[i+2].images.thumbnail.url + ")' src='images/polaroid.png' />" );
+			$('#gram3').append( "<img class='instagram-pic tiltBack_15' style='background-image: url(" + data[1].feed.data[i+2].images.thumbnail.url + ")' src='images/polaroid.png' />" );
 			gram3 = data[1].feed.data[i+2];
 			$('#four').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i+3].text + "</p>" );
 			four = data[0].feed[i+3];
 			$('#five').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i+4].text + "</p>" );
 			five = data[0].feed[i+=4];
-			$('#tube1').append( "<img class='tube' style='background-image: url(" + data[2].feed.items[i].snippet.thumbnails.medium.url + ")' src='images/tv1.png' />" );
+			$('#tube1').append( "<img class='tube tiltBack_7' style='background-image: url(" + data[2].feed.items[i].snippet.thumbnails.medium.url + ")' src='images/tv1.png' />" );
 			tube1 = data[2].feed.items[i];
+			$('#tube2').append( "<img class='tube tilt_15' style='background-image: url(" + data[2].feed.items[1].snippet.thumbnails.medium.url + ")' src='images/tv1.png' />" );
+			tube2 = data[2].feed.items[1];
+			$('#tube3').append( "<img class='tube tiltBack_7' style='background-image: url(" + data[2].feed.items[2].snippet.thumbnails.medium.url + ")' src='images/tv1.png' />" );
+			tube3 = data[2].feed.items[2];
+
 		}
 	});
 
@@ -30,10 +35,31 @@ $('#search-button').click(function(e) {
 	e.preventDefault();
 	var the_search = $('#search').val();
 	$.get("feeds/search?search="+the_search, function(data) {
-		$('#one, #two, #three, #four, #five, #six, #gram1, #gram2, #gram3').html("");
+		$('#one, #two, #three, #four, #five, #gram1, #gram2, #gram3, #tube1, #tube2, #tube3').html("");
 	
-		for (i=0; i<20; i++) {
-
+		for (i=0; i<5; i++) {
+			$('#one').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i].text + "</p>" );
+			one = data[0].feed[i];
+			$('#gram1').append( "<img class='instagram-pic tilt_15' style='background-image: url(" + data[1].feed.data[i].images.thumbnail.url + ")' src='images/polaroid.png' />" );
+			gram1 = data[1].feed.data[i];
+			$('#two').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i+1].text + "</p>" );
+			two = data[0].feed[i+1];
+			$('#gram2').append( "<img class='instagram-pic tiltBack_15' style='background-image: url(" + data[1].feed.data[i+1].images.thumbnail.url + ")' src='images/polaroid.png' />" );
+			gram2 = data[1].feed.data[i+1];
+			$('#three').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i+2].text + "</p>" );
+			three = data[0].feed[i+2];
+			$('#gram3').append( "<img class='instagram-pic tiltBack_15' style='background-image: url(" + data[1].feed.data[i+2].images.thumbnail.url + ")' src='images/polaroid.png' />" );
+			gram3 = data[1].feed.data[i+2];
+			$('#four').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i+3].text + "</p>" );
+			four = data[0].feed[i+3];
+			$('#five').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i+4].text + "</p>" );
+			five = data[0].feed[i+=4];
+			$('#tube1').append( "<img class='tube tiltBack_7' style='background-image: url(" + data[2].feed.items[i].snippet.thumbnails.medium.url + ")' src='images/tv1.png' />" );
+			tube1 = data[2].feed.items[i];
+			$('#tube2').append( "<img class='tube tilt_15' style='background-image: url(" + data[2].feed.items[1].snippet.thumbnails.medium.url + ")' src='images/tv1.png' />" );
+			tube2 = data[2].feed.items[1];
+			$('#tube3').append( "<img class='tube tiltBack_7' style='background-image: url(" + data[2].feed.items[2].snippet.thumbnails.medium.url + ")' src='images/tv1.png' />" );
+			tube3 = data[2].feed.items[2];
 		}
 	});
 });
@@ -147,7 +173,27 @@ $('#tube1').click(function (e) {
 	$('#videoDescription').empty().append(tube1.snippet.description);
 });
 
+$('#tube2').click(function (e) {
+	e.preventDefault();
+	console.log(e);
 
+	$('#youtubeModal').modal();
+	$('#videoTitle').empty().append(tube2.snippet.title);
+	$('#modalVideo').empty().append("<iframe width='575' height='323' src='//www.youtube.com/embed/" + tube2.id + "'></iframe>");
+	$('#youtube-user').empty().append(tube2.snippet.channelTitle);
+	$('#videoDescription').empty().append(tube2.snippet.description);
+});
+
+$('#tube3').click(function (e) {
+	e.preventDefault();
+	console.log(e);
+
+	$('#youtubeModal').modal();
+	$('#videoTitle').empty().append(tube3.snippet.title);
+	$('#modalVideo').empty().append("<iframe width='575' height='323' src='//www.youtube.com/embed/" + tube3.id + "'></iframe>");
+	$('#youtube-user').empty().append(tube3.snippet.channelTitle);
+	$('#videoDescription').empty().append(tube3.snippet.description);
+});
 
 
 
