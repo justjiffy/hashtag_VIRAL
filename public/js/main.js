a2fef13f6cb37c177275f8cb43f905a044ea1c56
Justin Hong <justinhong.j@gmail.com>
2015-10-08 17:15:17 -0700

    make template modals for twitter and instagram

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,10 @@
-var 
-
-$(document).ready( function() {
+$(document).ready(function() {
 	$.get('feeds/', function(data) {
 		for (i=0; i<5; i++) {
 			$('#one').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i].text + "</p>" );
-
+			one = data[0].feed[i];
 			$('#gram1').append( "<img class='instagram-pic tilt_15' style='background-image: url(" + data[1].feed.data[i].images.thumbnail.url + ")' src='images/polaroid.png' />" );
+			gram1 = data[1].feed.data[i];
 			$('#two').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i+1].text + "</p>" );
 			$('#gram2').append( "<img class='instagram-pic tiltBack_15' style='background-image: url(" + data[1].feed.data[i+1].images.thumbnail.url + ")' src='images/polaroid.png' />" );
 			$('#three').append( "<div class='tweet'><p class='twit'>" + data[0].feed[i+2].text + "</p>" );
@@ -40,10 +39,51 @@ $('#scene').parallax({
 });
 
 
-$('.t').click(function (e) {
+$('#one').click(function (e) {
 	e.preventDefault();
 	console.log(e);
 
 	$('#twitterModal').modal();
-	$('#tweetText').empty().append(e.target.innerText);
+	$('#tweetText').empty().append(one.text);
+	$('#twitProfilePic').empty().append("<img src='" + one.user.profile_image_url + "'>");
+	$('#twitUser').empty().append("<a href='//www.twitter.com/" + one.user.screen_name + "'>" + one.user.screen_name + "</a>");
 });
+
+$('#gram1').click(function (e) {
+	e.preventDefault();
+	console.log(e);
+
+	$('#instaModal').modal();
+	$('#gram-pic').empty().append("<img src='" + gram1.images.low_resolution.url + "'>");
+	$('#gramCaption').empty().append(gram1.caption.text);
+	$('#gramUserProfilePic').empty().append("<img style='width:48px;height' src='" + gram1.user.profile_picture + "'>");
+	$('#gramUser').empty().append("<a href='//www.instagram.com/" + gram1.user.username + "'>" + gram1.user.username + "</a>");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
